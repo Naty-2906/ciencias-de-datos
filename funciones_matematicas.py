0baf86da7c90a64148a62f0b1b49104c557e150c
Natalia Muñoz <natalia.munoz4@mail.udp.cl>
2025-12-09 12:47:28 -0300

    funcion suma

---
+++ funciones_matematicas.py
@@ -0,0 +1,8 @@
+def fn_suma (a, b):
+    total_suma: a + b
+    return total_suma
+if __name__== "__main__":
+    a = input('ingrese un numero')
+    b = input('ingrese otro numero')
+    resultado = fn_suma(int(a),int(b))
+    print("La suma es:", resultado)
